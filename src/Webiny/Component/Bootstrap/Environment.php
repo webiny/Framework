a296985368038e7eea31219748ab545169b1786b
sven <sven@webiny.com>
2015-02-08 15:38:20 +0000

    Removed ini_set for error reporting.

--- src/Webiny/Component/Bootstrap/Environment.php
+++ src/Webiny/Component/Bootstrap/Environment.php
@@ -221,10 +221,8 @@ class Environment
         );
         if (strtolower($errorReporting) == 'on') {
             error_reporting(E_ALL);
-            ini_set('display_errors', 1);
         } else {
             error_reporting(0);
-            ini_set('display_errors', 0);
         }
     }
 
