8d05d79c23a7de49623e5620fc3a2fcadf7c3026
Pavel910 <pavel@webiny.com>
2015-03-22 21:53:22 +0100

    Rest responseCode assignment in constructor

--- src/Webiny/Component/Rest/RestErrorException.php
+++ src/Webiny/Component/Rest/RestErrorException.php
@@ -57,6 +57,7 @@ class RestErrorException extends ExceptionAbstract
         $this->message = $message;
         $this->description = $description;
         $this->code = $code;
+        $this->responseCode = $responseCode;
     }
 
     /**
