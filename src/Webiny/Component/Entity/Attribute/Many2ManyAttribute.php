57f6909c50022692ab4505d2a7f8505eefc96e6f
Pavel910 <pavel@webiny.com>
2015-08-24 14:06:28 +0200

    Dirty flag fix on Many2Many attribute

--- src/Webiny/Component/Entity/Attribute/Many2ManyAttribute.php
+++ src/Webiny/Component/Entity/Attribute/Many2ManyAttribute.php
@@ -64,6 +64,7 @@ class Many2ManyAttribute extends CollectionAttributeAbstract
          */
         foreach ($item as $i) {
             $this->addedItems[] = $i;
+            $this->entity->setDirty(true);
         }
 
         return $this;
